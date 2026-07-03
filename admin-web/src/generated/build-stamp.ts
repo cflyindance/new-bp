9b29e9f9b4716c03aff85291357acc3bb5a4a0da
cafy <272735231@qq.com>
2026-07-03 16:39:08 +0800

    复杂场景的最后封板

--- admin-web/src/generated/build-stamp.ts
+++ admin-web/src/generated/build-stamp.ts
@@ -1,6 +1,2 @@
 /** 由 scripts/generate-build-stamp.mjs 在 build 时自动生成，请勿手改 */
-<<<<<<< HEAD
-export const BUILD_STAMP = "20260703082621";
-=======
-export const BUILD_STAMP = "20260703055020";
->>>>>>> 8e7d324e2b5b649068d1e5a05d156801e498b3da
+export const BUILD_STAMP = "20260703083859";
