d8adabb72a8bc3b80ef1e180fbf9e69ba41de322
cafy <272735231@qq.com>
2026-07-10 10:18:41 +0800

    emenupro

--- admin-web/src/generated/build-stamp.ts
+++ admin-web/src/generated/build-stamp.ts
@@ -1,6 +1,2 @@
 /** 由 scripts/generate-build-stamp.mjs 在 build 时自动生成，请勿手改 */
-<<<<<<< HEAD
-export const BUILD_STAMP = "20260710020658";
-=======
-export const BUILD_STAMP = "20260710014551";
->>>>>>> 6bde31c3d99f9e4ae9d068dbc211478fcb2b7a72
+export const BUILD_STAMP = "20260710021819";
