76e56414a2e74b740e4c89506e34ecbc870ba8ac
cafy <272735231@qq.com>
2026-09-15 10:09:28 +0800

    refactor: remove local image folder sync

---
+++ admin-web/dist/emenu-new/assets/index.8118689a.js
@@ -0,0 +1,1 @@
+import"./default.55118179.js";import{m}from"./antd.948aa529.js";m.config({maxCount:3});
---
+++ admin-web/dist/emenu-new/assets/index.866a12de.js
@@ -0,0 +1,1 @@
+import{j as r}from"./antd.948aa529.js";import{u as a}from"./index.42673f3c.js";const n="_systemSetting_header_1i4pc_1";var i={systemSetting_header:n};const d=e=>{const{t}=a(),{title:s}=e;return r("header",{className:i.systemSetting_header,children:t(`${s}`)})};export{d as H};
